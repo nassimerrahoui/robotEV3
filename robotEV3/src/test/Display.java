12aaa45b8ad890d5eaf8f8d33f513a15e5eda6ca
Grey <Grey@DESKTOP-UC9JHNA>
2018-01-10 16:35:12 +0100

    Classes de test

---
+++ robotEV3/src/test/Display.java
@@ -0,0 +1,13 @@
+package test;
+
+import lejos.hardware.Button;
+
+public class Display {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		Button.waitForAnyPress();
+	}
+
+}
