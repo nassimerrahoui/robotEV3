2f81b787e42de63f5ef2cb73e4660f3ce95a11f8
Thierry TAN <Chouwix@DESKTOP-SKBE2UL>
2018-01-12 14:05:19 +0100

    Test Gyro

---
+++ robotEV3/src/test/GyroscopeTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.robotics.SampleProvider;
+
+public class GyroscopeTest {
+	
+	public static void main(String[] args) {
+		Port port = LocalEV3.get().getPort("S1");
+
+		EV3GyroSensor gyro = new EV3GyroSensor(port);
+		
+		gyro.reset();
+		SampleProvider sample_provider = gyro.getAngleMode();
+		
+		// initialize an array of floats for fetching samples. 
+		// Ask the SampleProvider how long the array should be
+		float[] sample = new float[sample_provider.sampleSize()];
+
+		// fetch a sample
+		while(Button.ESCAPE.isUp()) {
+			gyro.fetchSample(sample, 0);
+			for (float f : sample) {
+				LCD.drawString("Angle : " + f +  " degres", 0, 3);
+			}
+
+
+		}
+		//close the sensor
+		gyro.close();
+
+		
+	}
+
+}
