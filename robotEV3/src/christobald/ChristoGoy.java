982cf6a7279844aa6b337c53eb4a4d008ba05def
Vincent <giardina.v@gmail.com>
2018-01-15 14:36:02 +0100

    Mode mode

--- robotEV3/src/christobald/ChristoGoy.java
+++ robotEV3/src/christobald/ChristoGoy.java
@@ -10,6 +10,15 @@ public class ChristoGoy {
 	public static MovementManager MM = new MovementManager(Motor.B, Motor.D);
 	public static EnvironmentManager EM = new EnvironmentManager("S2", Motor.A, "S3");
 	
+	public enum ChristoMode {
+		  WALL_FINDING,
+		  WALL_FOLLOWING,
+		  FORWARD_MOVING,
+		  QUARTER_TURN_AVOIDANCE,
+		  LEFT_CORRECTION,
+		  RIGHT_CORRECTION
+	}
+	
 	public static void forwardCheck() throws MoustachePressException
 	{
 		if(EM.isMoustachePressed()){
@@ -21,11 +30,68 @@ public class ChristoGoy {
 	
 	public static void main(String[] args) {
 		
-		BlockIO.displayMessage("Yo, I'm Chri 5.4");
+		BlockIO.displayMessage("Yo, I'm Chri 5.6");
 		BlockIO.waitUntilPress();
+		ChristoMode mode = ChristoMode.WALL_FINDING;
 		
 		while(Button.ESCAPE.isUp()) {
-			try {
+			switch (mode) {
+				case WALL_FINDING:
+					float distanceLeft = EM.getDistanceOn(EnvironmentManager.HeadDirection.LEFT);
+					float distanceRight = EM.getDistanceOn(EnvironmentManager.HeadDirection.RIGHT);
+					if(distanceLeft > 0.60) {
+						if(distanceRight < distanceLeft)
+							MM.rotate(60);
+						else
+							MM.rotate(-60);
+						mode = ChristoMode.FORWARD_MOVING;
+					}else {
+						mode = ChristoMode.WALL_FOLLOWING;
+					}
+					break;
+				case WALL_FOLLOWING:
+					try {
+						float distance = EM.getDistanceOn(EnvironmentManager.HeadDirection.LEFT);
+						BlockIO.displayMessage("Distance : " + distance);
+						if(distance < MIN_WALL_DISTANCE)
+						{
+							MM.rotate(-1 *CORRECTION_ANGLE);
+						}
+						else if(distance >= MAX_WALL_DISTANCE)
+						{
+							if(distance < 0.60) {
+								MM.rotate(CORRECTION_ANGLE);}
+							else {
+								MM.forward();
+								mode = ChristoMode.WALL_FINDING;
+							}
+						}
+						forwardCheck();
+					}
+					catch(MoustachePressException e)
+					{
+						mode = ChristoMode.QUARTER_TURN_AVOIDANCE;
+					}
+					break;
+				case FORWARD_MOVING:
+					try {
+						forwardCheck();
+					}catch(MoustachePressException e)
+					{
+						mode = ChristoMode.QUARTER_TURN_AVOIDANCE;
+					}
+					break;
+				case QUARTER_TURN_AVOIDANCE:
+					MM.stop();
+					MM.backward();
+					MM.rotate(60);
+					MM.stop();
+					mode = ChristoMode.WALL_FOLLOWING;
+					break;
+				default:
+					break;
+			}
+			/**try {
 				
 				float distance = EM.getDistanceOn(EnvironmentManager.HeadDirection.LEFT);
 				BlockIO.displayMessage("Distance : " + distance);
@@ -49,7 +115,7 @@ public class ChristoGoy {
 				MM.stop();
 				MM.backward();
 				MM.rotate(60);
-			}
+			}**/
 		}
 		EM.look(EnvironmentManager.HeadDirection.FRONT); 
 	}
--- robotEV3/src/test/GyroscopeTest.java
+++ robotEV3/src/test/GyroscopeTest.java
@@ -4,6 +4,7 @@ import lejos.hardware.Button;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.robotics.SampleProvider;
 import lejos.utility.Delay;
@@ -11,7 +12,7 @@ import lejos.utility.Delay;
 public class GyroscopeTest {
 	
 	public static void main(String[] args) {
-		Port port = LocalEV3.get().getPort("S1");
+		Port port = LocalEV3.get().getPort("S4");
 
 		EV3GyroSensor gyro = new EV3GyroSensor(port);
 		
@@ -21,19 +22,26 @@ public class GyroscopeTest {
 		// initialize an array of floats for fetching samples. 
 		// Ask the SampleProvider how long the array should be
 		float[] sample = new float[sample_provider.sampleSize()];
+		
+		Port sensorPort = LocalEV3.get().getPort("S1");            
+		EV3ColorSensor colorSensor = new EV3ColorSensor(sensorPort);
+		SampleProvider colorRGBSensor = colorSensor.getRGBMode();
+		int sampleSize = colorRGBSensor.sampleSize();            
+		float[] sampleColor = new float[sampleSize];
 
+		gyro.reset();
 		// fetch a sample
-		while(Button.ESCAPE.isUp()) {
-			gyro.fetchSample(sample, 0);
-			LCD.drawString("Angle : " + sample[0] +  " degres", 0, 3);
-			
-
-
+		while(Button.ESCAPE.isUp()){
+			sample_provider.fetchSample(sample, 0);
+			colorRGBSensor.fetchSample(sampleColor, 0);
+			//LCD.drawString("A : " + sample[0], 0, 3);
+			LCD.drawString("R : " + sampleColor[0], 0, 3);
+			LCD.drawString("G : " + sampleColor[1], 0, 4);
+			LCD.drawString("B : " + sampleColor[2], 0, 5);
+			Delay.msDelay(10);
 		}
 		//close the sensor
 		gyro.close();
-
-		
+		colorSensor.close();	
 	}
-
 }
